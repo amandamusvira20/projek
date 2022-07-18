b6b915e4622070b3302be6938f3cf0d72d9724e9
amandamusvira3120 <amandamsvra20@gmail.com>
2022-07-18 09:18:43 +0700

    reupload

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,3 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('pesan/{id}', [App\Http\Controllers\PesanController::class,'index']);
 Route::post('pesan/{id}', [App\Http\Controllers\PesanController::class,'pesan']);
-Route::get('checkout', [App\Http\Controllers\PesanController::class,'check_out']);
-Route::delete('checkout/{id}', [App\Http\Controllers\PesanController::class,'delete']);
-
-Route::get('konfirmasi-check-out', [App\Http\Controllers\PesanController::class,'konfirmasi']);
-
-Route::get('profile', [App\Http\Controllers\ProfileController::class,'index']);
-Route::post('profle', [App\Http\Controllers\ProfileSController::class,'update']);
-
-Route::get('history', [App\Http\Controllers\HistoryController::class,'index']);
-Route::get('history/{id}', [App\Http\Controllers\PesanController::class,'detail']);
