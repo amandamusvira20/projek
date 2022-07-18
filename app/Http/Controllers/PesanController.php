9ed2ff1e940baa199355d295c6f3cb4240947f0b
amandamusvira3120 <amandamsvra20@gmail.com>
2022-07-18 07:01:54 +0700

    commit pertama

--- app/Http/Controllers/PesanController.php
+++ app/Http/Controllers/PesanController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\Barang;
 use App\Models\Pesanan;
+use App\Models\User;
+use App\Models\DetailPesanan;
 use Illuminate\Http\Request;
 use Auth;
 use Carbon\Carbon;
@@ -13,38 +15,131 @@ class PesanController extends Controller
     {
         $this->middleware('auth');
     }
+
     public function index($id)
     {
-        $barang = Barang::where('id',$id)->first();
+    	$barang = Barang::where('id', $id)->first();
 
-        return view('pesan.index',compact('barang'));
+    	return view('pesan.index', compact('barang'));
     }
+
     public function pesan(Request $request, $id)
-    {   
-        $barang = Barang::where('id',$id)->first();
-        $tanggal = Carbon::now();
+    {	
+    	$barang = Barang::where('id', $id)->first();
+    	$tanggal = Carbon::now();
 
-        //simpan ke database pesanan
-        $pesanan = new Pesanan;
-        $pesanan->user_id = Auth::user()->id;
-        $pesanan->tanggal = $tanggal;
-        $pesanan->status = 0;
-        $pesanan->jumlah_harga = $barang->harga*$request->jumlah_pesan;
-        $pesanan->save();
+    	//validasi apakah melebihi stok
+    	if($request->jumlah_pesan > $barang->stok)
+    	{
+    		return redirect('pesan/'.$id);
+    	}
 
-        //simpan ke database detail pesanan
-        $pesanan_baru = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+    	//cek validasi
+    	$cek_pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+    	//simpan ke database pesanan
+    	if(empty($cek_pesanan))
+    	{
+    		$pesanan = new Pesanan;
+	    	$pesanan->user_id = Auth::user()->id;
+	    	$pesanan->tanggal = $tanggal;
+	    	$pesanan->status = 0;
+	    	$pesanan->jumlah_harga = 0;
+            $pesanan->kode = mt_rand(100, 999);
+	    	$pesanan->save();
+    	}
+    	
+    	//simpan ke database pesanan detail
+    	$pesanan_baru = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
 
-        $detailpesanan = new DetailPesanan;
-        $detailpesanan->barang_id = $barang->id;
-        $detailpesanan->pesanan_id = $pesanan_baru->id;
-        $detailpesanan->jumlah = $request->jumlah_pesan;
-        $detailpesanan->jumlah_harga = $barang->harga*$request->jumlah_pesan;
-        $detailpesanan->save();
+    	//cek pesanan detail
+    	$cek_pesanan_detail = DetailPesanan::where('barang_id', $barang->id)->where('pesanan_id', $pesanan_baru->id)->first();
+    	if(empty($cek_pesanan_detail))
+    	{
+    		$detail_pesanan = new DetailPesanan;
+	    	$detail_pesanan->barang_id = $barang->id;
+	    	$detail_pesanan->pesanan_id = $pesanan_baru->id;
+	    	$detail_pesanan->jumlah = $request->jumlah_pesan;
+	    	$detail_pesanan->jumlah_harga = $barang->harga*$request->jumlah_pesan;
+	    	$detail_pesanan->save();
+    	}else 
+    	{
+    		$pesanan_detail = PesananDetail::where('barang_id', $barang->id)->where('pesanan_id', $pesanan_baru->id)->first();
 
-       
+    		$pesanan_detail->jumlah = $pesanan_detail->jumlah+$request->jumlah_pesan;
 
+    		//harga sekarang
+    		$harga_pesanan_detail_baru = $barang->harga*$request->jumlah_pesan;
+	    	$pesanan_detail->jumlah_harga = $pesanan_detail->jumlah_harga+$harga_pesanan_detail_baru;
+	    	$pesanan_detail->update();
+    	}
 
+    	//jumlah total
+    	$pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+    	$pesanan->jumlah_harga = $pesanan->jumlah_harga+$barang->harga*$request->jumlah_pesan;
+    	$pesanan->update();
+    	
+        Alert::success('Pesanan Sukses Masuk Keranjang', 'Success');
+    	return redirect('check-out');
 
     }
+
+    public function check_out()
+    {
+        $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+ 	$pesanan_details = [];
+        if(!empty($pesanan))
+        {
+            $pesanan_details =DetailPesanan::where('pesanan_id', $pesanan->id)->get();
+
+        }
+        
+        return view('pesan.check_out', compact('pesanan', 'pesanan_details'));
+    }
+
+    public function delete($id)
+    {
+        $detail_pesanan =DetailPesanan::where('id', $id)->first();
+
+        $pesanan = Pesanan::where('id', $detail_pesanan->pesanan_id)->first();
+        $pesanan->jumlah_harga = $pesanan->jumlah_harga-$detail_pesanan->jumlah_harga;
+        $pesanan->update();
+
+
+        $detail_pesanan->delete();
+
+        Alert::error('Pesanan Sukses Dihapus', 'Hapus');
+        return redirect('check-out');
+    }
+
+    public function konfirmasi()
+    {
+        $user = User::where('id', Auth::user()->id)->first();
+
+        if(empty($user->alamat))
+        {
+            Alert::error('Identitasi Harap dilengkapi', 'Error');
+            return redirect('profile');
+        }
+
+        if(empty($user->nohp))
+        {
+            Alert::error('Identitasi Harap dilengkapi', 'Error');
+            return redirect('profile');
+        }
+
+        $pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+        $detail_pesanan = $pesanan->id;
+        $pesanan->status = 1;
+        $pesanan->update();
+
+        $pesanan_details =DetailPesanan::where('pesanan_id', $pesanan_id)->get();
+        foreach ($detail_pesanans as $detail_pesanans) {
+            $barang = Barang::where('id', $detail_pesanan->barang_id)->first();
+            $barang->stok = $barang->stok-$detail_pesanan->jumlah;
+            $barang->update();
+        }
+
+        Alert::success('Pesanan Sukses Check Out Silahkan Lanjutkan Proses Pembayaran', 'Success');
+        return redirect('history/'.$pesanan_id);
+}
 }
